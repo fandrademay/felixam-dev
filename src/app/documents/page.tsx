12a733a5858463a5b2dbecc51132131b4d148fc1
Felix <f.andrademay@gmail.com>
2025-05-20 16:59:23 +0100

    updated CV and documents page

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -13,32 +13,33 @@ export default function Home() {
                   target="_blank" rel="noopener noreferrer" download>
               <div className={styles.container}>
                 <Image src="/fea6-ProjectReport-2024-cover.png" 
-                        width={219} height={307} alt="fea6_ProjectReport_2024_cover.png"/>
+                        width={164.35} height={230.25} alt="fea6_ProjectReport_2024_cover.png"/>
               </div>
               <div className={styles.container}>
-                <Image src="download.svg" width={20} height={20} alt="Download"/> Download Dissertation
+                {/* <Image src="download.svg" width={20} height={20} alt="Download"/> */}
+                Download My Dissertation Thesis
               </div>
             </Link>
 
             <Link href={'FelixAM_cv.pdf'} className={styles.pdf_button} 
                   target="_blank" rel="noopener noreferrer" download>
               <div className={styles.container}>
-                <Image src="/FelixAM-cv-cover.png" width={219} height={307} alt="FelixAM-cv-cover.png"/>
+                <Image src="/FelixAM-cv-cover.png" width={164.35} height={230.25} alt="FelixAM-cv-cover.png"/>
               </div>
               <div className={styles.container}>
-                <Image src="download.svg" width={20} height={20} alt="Download"/>
-                Download CV
+                {/* <Image src="download.svg" width={20} height={20} alt="Download"/> */}
+                Download My Full CV
               </div>
             </Link>
 
             <Link href={'cbi-report.pdf'} className={styles.pdf_button}
                   target="_blank" rel="noopener noreferrer" download>
               <div className={styles.container}>
-                <Image src="/cbi-report-cover.png" width={219} height={307} alt="cbi-report-cover.png"/>
+                <Image src="/cbi-report-cover.png" width={164.35} height={230.25} alt="cbi-report-cover.png"/>
               </div>
               <div className={styles.container}>
-                <Image src="download.svg" width={20} height={20} alt="Download"/>
-                Download Paper
+                {/* <Image src="download.svg" width={20} height={20} alt="Download"/> */}
+                Download My <i>'Evaluating Metagenomic Assemblies'</i> Paper
               </div>
             </Link>
             
