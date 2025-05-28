81dbb03f8ef7c16684b9c94779870c29fbfcb3f1
Felix <f.andrademay@gmail.com>
2025-05-28 10:46:36 +0100

    added toggle theme button

--- src/app/components/ToggleThemeButton/index.tsx
+++ src/app/components/ToggleThemeButton/index.tsx
@@ -4,6 +4,7 @@
 import React, { useContext } from 'react';
 import { useTheme } from '@designcise/next-theme-toggle';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import styles from "../../page.module.css";
 
@@ -12,13 +13,19 @@ export default function ToggleThemeButton() {
   const { toggleTheme } = useTheme()
 
   return (
-    <div className={styles.toggle_switch}>
-      <label className={styles.switch}>
-        <input type='checkbox'/>
-        <span className={styles.slider} onClick={toggleTheme}></span>
-      </label>
-      <h3 className={styles.h3}>Toggle Theme</h3>
-    </div>  
-    // <button onClick={toggleTheme}>Toggle Theme</button>
+    // <div className={styles.toggle_switch}>
+    //   <label className={styles.switch}>
+    //     <input type='checkbox'/>
+    //     <span className={styles.slider} onClick={toggleTheme}></span>
+    //   </label>
+    //   <h3 className={styles.h3}>Toggle Theme</h3>
+    // </div>  
+    <div className={styles.ctas}>
+      <a className={styles.major} onClick={toggleTheme}>
+          <Image className={styles.inverting} src="/images/icons/theme.svg" 
+                  width={20} height={20} alt="Change Theme"/>
+            Change Theme
+      </a>
+    </div>
   );
 }
