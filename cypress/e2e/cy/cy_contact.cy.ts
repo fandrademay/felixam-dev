ff6c1a3bcca25b63e192dfa9822b6dba6a9a853a
Felix <f.andrademay@gmail.com>
2025-07-03 15:30:02 +0100

    synced package-lock, removed social button tests

--- cypress/e2e/cy/cy_contact.cy.ts
+++ cypress/e2e/cy/cy_contact.cy.ts
@@ -6,7 +6,7 @@ describe('CY Contact Page Content', () => {
   it('should load the contact page', () => {
     cy.get('h1').contains("Cysylltu").should('be.visible')
     cy.get('h2').contains("E-Bost").should('be.visible')
-    cy.get('h2').contains("Cyfryngau Cymdeithasol").should("be.visible")
+    cy.get('h2').contains("Cyfryngau Cymdeithasol").should("be.hidden")
   })
 
   it('should load the email button', () => {
@@ -15,10 +15,10 @@ describe('CY Contact Page Content', () => {
     cy.get('a').contains("f.andrademay@gmail.com").should('have.attr', 'href', "mailto:f.andrademay@gmail.com")
   })
 
-  it('should load the social media buttons', () => {
-    cy.get(':nth-child(5) > [href="https://www.linkedin.com/in/felixAmay"]').find('img').should('be.visible');
-    cy.get(':nth-child(5) > [href="https://www.linkedin.com/in/felixAmay"]').should('be.visible');
-    cy.get(':nth-child(5) > [href="https://github.com/fandrademay"]').should('be.visible');
-    cy.get(':nth-child(5) > [href="https://github.com/fandrademay"]').find('img').should('be.visible');
-  })
+  // it('should load the social media buttons', () => {
+  //   cy.get(':nth-child(5) > [href="https://www.linkedin.com/in/felixAmay"]').find('img').should('be.hidden');
+  //   cy.get(':nth-child(5) > [href="https://www.linkedin.com/in/felixAmay"]').should('be.hidden');
+  //   cy.get(':nth-child(5) > [href="https://github.com/fandrademay"]').should('be.hidden');
+  //   cy.get(':nth-child(5) > [href="https://github.com/fandrademay"]').find('img').should('be.hidden');
+  // })
 })
